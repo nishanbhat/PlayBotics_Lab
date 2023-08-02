6d1b48b152c6e685e15c266ed6ed5fd5cf913463
nishanbhat <rananishanbhat@gmail.com>
2023-08-02 13:56:30 +0545

    homepage responsive done

--- script.js
+++ script.js
@@ -11,30 +11,30 @@ function closeSearch() {
 new WOW().init();
 
 // Owlcarousel
-$(document).ready(function(){
+$(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-  	loop:true,
-    margin:10,
-    nav:true,
-	autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
     center: true,
     navText: [
-	    "<i class='fa fa-angle-left'></i>",
-	    "<i class='fa fa-angle-right'></i>"
-	],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:3
-        }
-    }
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      },
+    },
   });
 });
 
